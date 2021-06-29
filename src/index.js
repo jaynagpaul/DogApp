c94fc29c0d544caa378df2756b869aaaf1af8dfa
Jay Nagpaul <jaynagpaul@gmail.com>
2021-06-29 18:57:23 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Demo from "./demo";
-import Chart from "./chart";
-import NavBar from "./navbar";
-import NewDog from "./newdog";
-import Welcome from "./welcome";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Questionaire from './questionaire';
+import Chart from './chart';
+import NavBar from './navbar';
+import NewDog from './newdog';
+import Welcome from './welcome';
 function App() {
-  const vals = React.useState(3);
+  const vals = React.useState(3); // Default to Welcome screen
   const [value, setValue] = vals;
   return (
     <>
-      <div style={{ paddingBottom: "65px" }}>
-        {value === 0 && <Demo />}
+      <div style={{ paddingBottom: '65px' }}>
+        {value === 0 && <Questionaire />}
         {value === 1 && <Chart />}
         {value === 2 && <NewDog />}
         {value === 3 && <Welcome setval={setValue} />}
@@ -24,5 +24,5 @@ ReactDOM.render(
   <div>
     <App />
   </div>,
-  document.querySelector("#root")
+  document.querySelector('#root')
 );
